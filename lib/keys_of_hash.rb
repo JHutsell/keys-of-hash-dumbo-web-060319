59b5d64e8d644496735e206e09a1c5ed2adf740a
Justin Hutsell <hutselljustin@gmail.com>
2019-05-30 22:50:02 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     result = []
-    @self.each do |k, v|
+    self.each do |k, v|
       if arguments.include?(v)
         result << k
       end
